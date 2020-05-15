2f3f07b3203d9fdf4159428c3846638fa6fbc6e5
Anastasiya <amolchkova@mail.ru>
2020-05-15 16:24:29 +0700

    Добавьте файлы проекта.

---
+++ print_with_pauses.cpp
@@ -0,0 +1,22 @@
+﻿/*Программа выводит на экран сообщение в "телеграфном" стиле: 
+буквы сообщения должны появляться по одной, с некоторой задержкой  */
+
+#include <iostream>
+#include <string>
+#include <Windows.h> // для Sleep
+
+int main()
+{
+	setlocale(LC_CTYPE, "rus");  // для вывода сообщений на кириллице
+
+	const std::string text{ "Привет, эта программа печатает текст с паузами" };
+
+	for (unsigned i{ 0 }; i < text.length(); i++)
+	{
+		std::cout << text[i];
+		Sleep(250);              // функция задержки программы для Windows, ждём указанное количество милисекунд
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
